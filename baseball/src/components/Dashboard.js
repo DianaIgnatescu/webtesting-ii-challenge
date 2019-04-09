b8f3f61e1efa11db668772c4ca5205369bbb45fc
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-09 15:53:27 +0100

    MVP: Add data attributes for buttons

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -4,10 +4,10 @@ const Dashboard = (props) => {
   return (
     <div>
       <h2>Dashboard</h2>
-      <button onClick={props.handleStrike}>Strike</button>
-      <button onClick={props.handleBall}>Ball</button>
-      <button onClick={props.handleFoul}>Foul</button>
-      <button onClick={props.handleHit}>Hit</button>
+      <button data-testid="strike-button" onClick={props.handleStrike}>Strike</button>
+      <button data-testid="ball-button" onClick={props.handleBall}>Ball</button>
+      <button data-testid="foul-button" onClick={props.handleFoul}>Foul</button>
+      <button data-testid="hit-button" onClick={props.handleHit}>Hit</button>
     </div>
   );
 };
