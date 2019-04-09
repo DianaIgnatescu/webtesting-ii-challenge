198cf4b335b924c92a1f42c3df4dc7492cf10e70
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-09 12:40:59 +0100

    MVP: Check that App renders without crashing

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+
+describe('<App />', () => {
+  it('should render without crashing', () => {
+    rtl.render(<App />);
+  });
+
 });
